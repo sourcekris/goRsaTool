38f21b852a3e6dab5f716c97faacd1254c83b5c7
Kris Hunt <sourcekris@users.noreply.github.com>
2017-10-01 20:29:06 +1100

    fix GCD.

--- libnum/egcd.go
+++ libnum/egcd.go
@@ -4,5 +4,5 @@ import fmp "github.com/sourcekris/goflint"
 
 // Returns a Fmpz GCD of a and b or 1 if a and b are co-prime
 func FindGcd(a, b *fmp.Fmpz) *fmp.Fmpz {
-  return new(fmp.Fmpz).GCD(nil, nil, a, b)
+  return new(fmp.Fmpz).GCD(a, b)
 }
