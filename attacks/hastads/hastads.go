afd2c6e524769708b2247221a865208ddc08757c
kris <kris@sneakerz.org>
2020-05-11 22:54:58 +1000

    add a timeout to the hastads attack

--- attacks/hastads/hastads.go
+++ attacks/hastads/hastads.go
@@ -1,7 +1,10 @@
 package hastads
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/sourcekris/goRsaTool/keys"
 	"github.com/sourcekris/goRsaTool/ln"
@@ -9,8 +12,28 @@ import (
 	fmp "github.com/sourcekris/goflint"
 )
 
+// timeout puts a limit on how long fermat should attempt to find a solution.
+var timeout = time.Minute * 5
+
 // name is the name of this attack.
 const name = "hastads"
+
+func hastads(ch chan bool, n, e, c, pt *fmp.Fmpz) {
+	m := new(fmp.Fmpz)
+	pow := new(fmp.Fmpz)
+	original := new(fmp.Fmpz).Set(c)
+
+	for {
+		m.Root(c, int32(e.Int64()))
+		pow.Exp(m, e, n)
+
+		if pow.Cmp(original) == 0 {
+			pt.Set(m)
+			ch <- true
+		}
+		c.Add(c, n)
+	}
+}
 
 // Attack implements the Hastads attack.
 func Attack(ts []*keys.RSA) error {
@@ -23,20 +46,25 @@ func Attack(ts []*keys.RSA) error {
 		return fmt.Errorf("%s failed - ciphertext needs to be provided for this attack", name)
 	}
 
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	ch := make(chan bool)
+
 	c := ln.BytesToNumber(t.CipherText)
-	m := new(fmp.Fmpz)
-	pow := new(fmp.Fmpz)
+	pt := new(fmp.Fmpz)
 
-	original := new(fmp.Fmpz).Set(c)
+	if t.Verbose {
+		log.Printf("%s attempt beginning with timeout %v", name, timeout)
+	}
+	go hastads(ch, t.Key.N, t.Key.PublicKey.E, c, pt)
 
-	for {
-		m.Root(c, int32(t.Key.PublicKey.E.Int64()))
-		pow.Exp(m, t.Key.PublicKey.E, t.Key.N)
-
-		if pow.Cmp(original) == 0 {
-			t.PlainText = ln.NumberToBytes(m)
-			return nil
-		}
-		c.Add(c, t.Key.N)
+	select {
+	case <-ch:
+		t.PlainText = ln.NumberToBytes(pt)
+		return nil
+	case <-ctx.Done():
+		return fmt.Errorf("%s failed to recover the plaintext", name)
 	}
 }
