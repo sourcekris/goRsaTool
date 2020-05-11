e6cdea6ccb9e0738818162384954f3ba3dc37120
kris <kris@sneakerz.org>
2020-05-11 22:29:02 +1000

    add a timeout to fermat factorization

--- attacks/fermat/fermat.go
+++ attacks/fermat/fermat.go
@@ -1,11 +1,33 @@
 package fermat
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/sourcekris/goRsaTool/keys"
 	"github.com/sourcekris/goRsaTool/ln"
 
 	fmp "github.com/sourcekris/goflint"
 )
+
+// timeout puts a limit on how long fermat should attempt to find a solution.
+var timeout = time.Minute * 5
+
+// name is the name of this attack.
+const name = "fermat factorization"
+
+func fermat(ch chan bool, a, b, b2, c, n *fmp.Fmpz) {
+	for c.Cmp(b2) != 0 {
+		a.Add(a, ln.BigOne)
+		b2.Mul(a, a).Sub(b2, n)
+		b.Sqrt(b2)
+		c.Mul(b, b)
+	}
+
+	ch <- true
+}
 
 // Attack implements the Fermat Factorization attack.
 func Attack(ts []*keys.RSA) error {
@@ -15,6 +37,12 @@ func Attack(ts []*keys.RSA) error {
 		return nil
 	}
 
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	ch := make(chan bool)
+
 	a := new(fmp.Fmpz).Sqrt(t.Key.N)
 	b := new(fmp.Fmpz).Set(a)
 	b2 := new(fmp.Fmpz).Mul(a, a)
@@ -22,13 +50,18 @@ func Attack(ts []*keys.RSA) error {
 
 	c := new(fmp.Fmpz).Mul(b, b)
 
-	for c.Cmp(b2) != 0 {
-		a.Add(a, ln.BigOne)
-		b2.Mul(a, a).Sub(b2, t.Key.N)
-		b.Sqrt(b2)
-		c.Mul(b, b)
+	if t.Verbose {
+		log.Printf("%s attempt beginning with timeout %v", name, timeout)
 	}
 
-	t.PackGivenP(new(fmp.Fmpz).Add(a, b))
+	go fermat(ch, a, b, b2, c, t.Key.N)
+
+	select {
+	case <-ch:
+		t.PackGivenP(new(fmp.Fmpz).Add(a, b))
+	case <-ctx.Done():
+		return fmt.Errorf("%s failed to factorize the key in the given time", name)
+	}
+
 	return nil
 }
