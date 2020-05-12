79d41db0d67ffccee5983695aff6b8e03f70a36a
kris <kris@sneakerz.org>
2020-05-12 23:20:54 +1000

    switch factordb attack to use factordb.com/api instead of HTML parsing

--- attacks/factordb/factordb.go
+++ attacks/factordb/factordb.go
@@ -1,80 +1,35 @@
 package factordb
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
 	"regexp"
-	"strings"
 	"time"
 
 	"github.com/sourcekris/goRsaTool/keys"
 	"github.com/sourcekris/goRsaTool/ln"
-	"github.com/sourcekris/goRsaTool/utils"
-	"golang.org/x/net/html"
-
-	fmp "github.com/sourcekris/goflint"
 )
 
 // name is the name of this attack.
 const name = "factordb factorization"
 
 var (
-	base       = "http://www.factordb.com/"
+	base       = "http://www.factordb.com/api?query="
 	query      = "index.php?query="
 	linkPrefix = "index.php?id="
 	// eqRE is a regex that matches equations in the form x^y-z
 	eqRE = regexp.MustCompile(`^(\d+)\^(\d+)\-(\d+)$`)
 )
 
-// solveforP extracts components of an equation we get back from factordb and solve it
-func solveforP(equation string) *fmp.Fmpz {
-	eq := eqRE.FindStringSubmatch(equation)
-	if len(eq) == 4 {
-		x, _ := new(fmp.Fmpz).SetString(eq[1], 10)
-		y, _ := new(fmp.Fmpz).SetString(eq[2], 10)
-		z, _ := new(fmp.Fmpz).SetString(eq[3], 10)
-		x.Exp(x, y, nil).Sub(x, z)
-		return x
-	}
-	return ln.BigZero
+type factorDB struct {
+	ID      string          `json:"id"`
+	Status  string          `json:"status"`
+	Factors [][]interface{} `json:"factors"`
 }
 
-func getHTMLAttr(r io.Reader, attr, prefix string, match int) (string, error) {
-	var count int
-	z := html.NewTokenizer(r)
-	for {
-		tt := z.Next()
-		switch tt {
-		case html.ErrorToken:
-			return "", z.Err()
-		case html.StartTagToken:
-			for {
-				k, v, _ := z.TagAttr()
-				if string(k) == attr {
-					switch {
-					case prefix == "":
-						if count == match {
-							return string(v), nil
-						}
-						count++
-					case prefix != "" && strings.HasPrefix(string(v), prefix):
-						if count == match {
-							return string(v), nil
-						}
-						count++
-					}
-				}
-
-				if k == nil {
-					break
-				}
-			}
-		}
-	}
-}
-
-// Attack factors an RSA Public Key using FactorDB.
+// Attack factors an RSA Public Key using FactorDB API.
 func Attack(ts []*keys.RSA) error {
 	t := ts[0]
 	if t.Key.D != nil {
@@ -86,65 +41,38 @@ func Attack(ts []*keys.RSA) error {
 		Timeout: 15 * time.Second,
 	}
 
-	resp, err := httpClient.Get(base + query + t.Key.N.String())
+	resp, err := httpClient.Get(base + t.Key.N.String())
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("failed to lookup modulus - unexpected http code: %d", resp.StatusCode)
+		return fmt.Errorf("%s failed to lookup modulus - unexpected http code: %d", name, resp.StatusCode)
 	}
 
-	// Parse the response Body looking for links and the respective attributes.
-	id, err := getHTMLAttr(resp.Body, "href", linkPrefix, 0)
+	js, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 
-	// Extract the primes from the second url
-	r1, err := httpClient.Get(base + id)
-	if err != nil {
-		return err
-	}
-	defer r1.Body.Close()
-
-	primeID, err := getHTMLAttr(r1.Body, "href", linkPrefix, 1)
-	if err != nil {
+	fdb := factorDB{}
+	if err := json.Unmarshal(js, &fdb); err != nil {
 		return err
 	}
 
-	r2, err := httpClient.Get(base + primeID)
-	if err != nil {
-		return err
-	}
-	defer r2.Body.Close()
-
-	p, err := getHTMLAttr(r2.Body, "value", "", 0)
-	if err != nil {
-		return err
-	}
-
-	// check if the returned values are all digits
-	if !utils.IsInt(p) {
-		// Try solve them as equations of the form x^y-z
-		tmpP := solveforP(p)
-		if tmpP.Cmp(ln.BigZero) == 0 {
-			return fmt.Errorf("prime p could not be resolved: %v", p)
+	if fdb.Status == "FF" {
+		if len(fdb.Factors) < 1 {
+			return fmt.Errorf("%s failed due to an unknown error - modulus status is FF but primes were not found", name)
+		}
+		p, ok := fdb.Factors[0][0].(string)
+		if !ok {
+			return fmt.Errorf("%s failed asserting type of factor to be a string: %v", name, fdb.Factors[0][0])
 		}
 
-		t.PackGivenP(tmpP)
+		t.PackGivenP(ln.FmpString(p))
 		return nil
 	}
 
-	keyP, ok := new(fmp.Fmpz).SetString(p, 10)
-	if !ok {
-		return err
-	}
-
-	if keyP.Cmp(t.Key.N) == 0 {
-		return fmt.Errorf("%s failed - factordb does not know the factors for %v", name, t.Key.N)
-	}
-	t.PackGivenP(keyP)
-	return nil
+	return fmt.Errorf("%s failed - the modulus is not fully factored on factordb (status = %s)", name, fdb.Status)
 }
--- attacks/factordb/factordb_test.go
+++ attacks/factordb/factordb_test.go
@@ -1,12 +1,10 @@
 package factordb
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/sourcekris/goRsaTool/keys"
-	"github.com/sourcekris/goRsaTool/ln"
-	
+
 	fmp "github.com/sourcekris/goflint"
 )
 
@@ -26,116 +24,5 @@ func TestFactorDB(t *testing.T) {
 
 	if targetRSA.Key.D.Cmp(d) != 0 {
 		t.Errorf("got %v wanted %v for d\n", targetRSA.Key.D, d)
-	}
-}
-
-func TestSolveForP(t *testing.T) {
-	tt := []struct {
-		name    string
-		eq      string
-		want    *fmp.Fmpz
-		wantErr bool
-	}{
-		{
-			name: "expected equation format",
-			eq:   "2^10-1",
-			want: fmp.NewFmpz(1023),
-		},
-		{
-			name:    "partial equation format",
-			eq:      "2^10",
-			want:    fmp.NewFmpz(-1),
-			wantErr: true,
-		},
-		{
-			name:    "unexpected equation format",
-			eq:      "2+4-1",
-			want:    fmp.NewFmpz(-1),
-			wantErr: true,
-		},
-		{
-			name:    "empty equation",
-			eq:      "",
-			want:    fmp.NewFmpz(-1),
-			wantErr: true,
-		},
-	}
-
-	for _, tc := range tt {
-		got := solveforP(tc.eq)
-		if tc.want.Cmp(got) != 0 && !tc.wantErr {
-			t.Errorf("solveforP: %s failed - got %v want %v", tc.name, got, tc.want)
-		}
-
-		if got.Cmp(ln.BigZero) == 0 && !tc.wantErr {
-			t.Errorf("solveforP: %s unexpected error", tc.name)
-		}
-
-		if got.Cmp(ln.BigZero) != 0 && tc.wantErr {
-			t.Errorf("solveforP: %s unexpected result when expected error: %v", tc.name, got)
-		}
-	}
-}
-
-func TestGetHTMLAttr(t *testing.T) {
-	tt := []struct {
-		name    string
-		html    string
-		attr    string
-		prefix  string
-		match   int
-		want    string
-		wantErr bool
-	}{
-		{
-			name:   "expected format with specific prefix",
-			html:   `<img src="meme.gif"><a href="/p/link1.html">Link</a><p>text</p><a href="/p/link2.html">`,
-			attr:   "href",
-			prefix: "/p/",
-			match:  1,
-			want:   "/p/link2.html",
-		},
-		{
-			name:  "expected format with no prefix",
-			html:  `<img src="meme.gif"><input value="12345">Link</a><p>text</p><input src="/" value="5678">`,
-			attr:  "value",
-			match: 1,
-			want:  "5678",
-		},
-		{
-			name:    "expected format with no prefix but match out of range",
-			html:    `<img src="meme.gif"><input value="12345">Link</a><p>text</p><input src="/" value="5678">`,
-			attr:    "value",
-			match:   2,
-			wantErr: true,
-		},
-		{
-			name:    "expected format with wrong prefix",
-			html:    `<img src="meme.gif"><input value="12345">Link</a><p>text</p><input src="/" value="5678">`,
-			attr:    "value",
-			prefix:  "wrong",
-			wantErr: true,
-		},
-		{
-			name:    "attr not found",
-			html:    `<img src="meme.gif"><input value="12345">Link</a><p>text</p><input src="/" value="5678">`,
-			attr:    "chicken",
-			wantErr: true,
-		},
-	}
-
-	for _, tc := range tt {
-		r := strings.NewReader(tc.html)
-		got, err := getHTMLAttr(r, tc.attr, tc.prefix, tc.match)
-		if err != nil && !tc.wantErr {
-			t.Errorf("getHTMLAttr() failed: %s - got error when none expected: %v", tc.name, err)
-		}
-		if err == nil && tc.wantErr {
-			t.Errorf("getHTMLAttr() failed: %s - expected error but got none", tc.name)
-		}
-		if got != tc.want && !tc.wantErr {
-			t.Errorf("getHTMLAttr() failed: %s - got %v wanted %v", tc.name, got, tc.want)
-		}
-
 	}
 }
