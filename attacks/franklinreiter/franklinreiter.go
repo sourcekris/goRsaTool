db5e2967ff7c1abce884400593d22be7839e2e5c
Kris <kris@sneakerz.org>
2020-05-31 20:25:40 +1000

    reorg signature attack module
    Was a reorg in order to try and catch panics but since the panics are from segfaults in cgo i might just leave it at that

--- attacks/franklinreiter/franklinreiter.go
+++ attacks/franklinreiter/franklinreiter.go
@@ -13,6 +13,53 @@ import (
 // name is the name of this attack.
 const name = "franklin reiter related message attack"
 
+type sigAttack struct {
+	cs []*fmp.Fmpz
+	ss []*fmp.Fmpz
+	n  *fmp.Fmpz
+	e  int
+}
+
+// attempt runs the attack attempt itself.
+func (s *sigAttack) attempt(v bool) []byte {
+	// f = (x-s1+s2)^e - c1
+	f := fmp.NewFmpzModPoly(s.n).SetCoeffUI(1, 1)
+	f.Sub(f, fmp.NewFmpzModPoly(s.n).SetCoeff(0, s.ss[1])).Add(f, fmp.NewFmpzModPoly(s.n).SetCoeff(0, s.ss[0])).Pow(f, s.e)
+	f.Sub(f, fmp.NewFmpzModPoly(s.n).SetCoeff(0, s.cs[0]))
+
+	// g = x^e-c2
+	g := fmp.NewFmpzModPoly(s.n).SetCoeffUI(1, 1)
+	g.Pow(g, s.e).Sub(g, fmp.NewFmpzModPoly(s.n).SetCoeff(0, s.cs[1]))
+
+	a := fmp.NewFmpzModPoly(f.GetMod()).Set(f)
+	b := fmp.NewFmpzModPoly(g.GetMod()).Set(g)
+
+	zero := fmp.NewFmpzModPoly(s.n).Zero()
+	rp := fmp.NewFmpzModPoly(s.n)
+
+	if v {
+		log.Printf("%s beginning, this can sometimes crash, try it again if it does.", name)
+	}
+
+	var r *fmp.FmpzModPoly
+	for {
+		_, r = a.DivRem(b)
+
+		if r.Equal(zero) {
+			co0 := rp.GetCoeff(0)
+			co1 := rp.GetCoeff(1)
+
+			q, _ := fmp.NewFmpzModPoly(s.n).SetCoeff(0, ln.BigOne).DivRem(fmp.NewFmpzModPoly(s.n).SetCoeff(0, co1))
+			q.MulScalar(q, ln.BigNOne).MulScalar(q, co0)
+			return ln.NumberToBytes(q.GetCoeff(0))
+		}
+
+		rp.Set(r)
+		a.Set(b)
+		b.Set(r)
+	}
+}
+
 // Attack implements the franklin reiter related message attack against two keys.
 func Attack(ks []*keys.RSA) error {
 	if len(ks) != 2 {
@@ -27,48 +74,16 @@ func Attack(ks []*keys.RSA) error {
 		return fmt.Errorf("%s requires each key has a corresponding ciphertext", name)
 	}
 
-	c1 := ln.BytesToNumber(ks[0].CipherText)
-	c2 := ln.BytesToNumber(ks[1].CipherText)
-	s1 := ln.BytesToNumber(ks[0].KnownPlainText)
-	s2 := ln.BytesToNumber(ks[1].KnownPlainText)
-	e := ks[0].Key.PublicKey.E.GetInt()
-	n := ks[0].Key.N
-
-	// f = (x-s1+s2)^e - c1
-	f := fmp.NewFmpzModPoly(n).SetCoeffUI(1, 1)
-	f.Sub(f, fmp.NewFmpzModPoly(n).SetCoeff(0, s2)).Add(f, fmp.NewFmpzModPoly(n).SetCoeff(0, s1)).Pow(f, e)
-	f.Sub(f, fmp.NewFmpzModPoly(n).SetCoeff(0, c1))
-
-	// g = x^e-c2
-	g := fmp.NewFmpzModPoly(n).SetCoeffUI(1, 1)
-	g.Pow(g, e).Sub(g, fmp.NewFmpzModPoly(n).SetCoeff(0, c2))
-
-	a := fmp.NewFmpzModPoly(f.GetMod()).Set(f)
-	b := fmp.NewFmpzModPoly(g.GetMod()).Set(g)
-
-	zero := fmp.NewFmpzModPoly(n).Zero()
-	rp := fmp.NewFmpzModPoly(n)
-
-	if ks[0].Verbose {
-		log.Printf("%s beginning, this can sometimes crash, try it again if it does.", name)
+	sa := &sigAttack{n: ks[0].Key.N, e: ks[0].Key.PublicKey.E.GetInt()}
+	for i := 0; i < 2; i++ {
+		sa.ss = append(sa.ss, ln.BytesToNumber(ks[i].KnownPlainText))
+		sa.cs = append(sa.cs, ln.BytesToNumber(ks[i].CipherText))
 	}
 
-	var r *fmp.FmpzModPoly
-	for {
-		_, r = a.DivRem(b)
-
-		if r.Equal(zero) {
-			co0 := rp.GetCoeff(0)
-			co1 := rp.GetCoeff(1)
-
-			q, _ := fmp.NewFmpzModPoly(n).SetCoeff(0, ln.BigOne).DivRem(fmp.NewFmpzModPoly(n).SetCoeff(0, co1))
-			q.MulScalar(q, ln.BigNOne).MulScalar(q, co0)
-			ks[0].PlainText = ln.NumberToBytes(q.GetCoeff(0))
-			return nil
-		}
-
-		rp.Set(r)
-		a.Set(b)
-		b.Set(r)
+	if res := sa.attempt(ks[0].Verbose); res != nil {
+		ks[0].PlainText = res
+		return nil
 	}
+
+	return fmt.Errorf("%s failed to recover the plaintext", name)
 }
