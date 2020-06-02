d728c90d19a41be7b588f904bcfa6889f55f06fa
Kris <kris@sneakerz.org>
2020-06-02 19:46:49 +1000

    commit initial version of dixons factorization
    naive implementation in a for loop. This is not suitable for factoring large composites though

--- attacks/dixons/dixons.go
+++ attacks/dixons/dixons.go
@@ -1,12 +1,45 @@
 package dixons
 
-import "github.com/sourcekris/goRsaTool/keys"
+import (
+	"fmt"
+
+	"github.com/sourcekris/goRsaTool/keys"
+	"github.com/sourcekris/goRsaTool/ln"
+
+	fmp "github.com/sourcekris/goflint"
+)
 
 // name is the name of this attack.
-const name = "dixons factorization"
+const name = "dixon's factorization"
 
-// Attack implements the Dixon's factorization method (maybe one day)
+var (
+	base = []*fmp.Fmpz{ln.BigTwo, ln.BigThree, ln.BigFive, ln.BigEleven,
+		fmp.NewFmpz(13), fmp.NewFmpz(17), fmp.NewFmpz(19), fmp.NewFmpz(23)}
+)
+
+// Attack implements the Dixon's factorization method.
 func Attack(ks []*keys.RSA) error {
-	// Not Yet Implemented.
-	return nil
+	var (
+		k = ks[0]
+		n = k.Key.PublicKey.N
+		i = new(fmp.Fmpz).Sqrt(n)
+	)
+
+	for i.Cmp(n) < 0 {
+		for _, j := range base {
+			lhs := new(fmp.Fmpz).ExpXIM(i, 2, n)
+			rhs := new(fmp.Fmpz).ExpXIM(j, 2, n)
+
+			if lhs.Equals(rhs) {
+				f := new(fmp.Fmpz).GCD(new(fmp.Fmpz).Sub(i, j), n)
+				if !f.Equals(ln.BigOne) {
+					k.PackGivenP(f)
+					return nil
+				}
+			}
+		}
+		i.AddI(1)
+	}
+
+	return fmt.Errorf("%s failed to find a factor", name)
 }
